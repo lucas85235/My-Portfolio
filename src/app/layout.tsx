2223d9ad8374410b211d1f3c2926a5715b0942f6
Lucas Lima <lucas85235@gmail.com>
2025-10-24 12:35:49 -0400

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { Providers } from './providers';
 import { Navbar } from '@/components/Navbar';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           </div>
 
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
